42fe03e4361af0803191fb72d0d2ac5de2e02f75
terrymagnusdrever <tdrever@pm.me>
2025-05-31 11:00:10 -0700

    CleanUp: Extract event application logic, re-use it instead of sending internal events.

--- server/src/systems/setup_redrive_event_log.rs
+++ server/src/systems/setup_redrive_event_log.rs
@@ -1,11 +1,16 @@
-use crate::find_latest_log_file_in_folder;
-use crate::systems::{FanOutEventSender, LoggedEvent};
-use bevy::prelude::Res;
+use crate::systems::{LoggedEvent, apply_event};
+use crate::{NeedsWorldBroadcast, find_latest_log_file_in_folder};
+use bevy::prelude::ResMut;
+use shared::{ServerGameState, ServerHistoryState};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use tracing::info;
 
-pub fn setup_redrive_event_log(sender: Res<FanOutEventSender>) {
+pub fn setup_redrive_event_log(
+    mut server_game_state: ResMut<ServerGameState>,
+    mut server_history_state: ResMut<ServerHistoryState>,
+    mut needs_world_broadcast: ResMut<NeedsWorldBroadcast>,
+) {
     let Some(log_path) = find_latest_log_file_in_folder("./_out/event_stream") else {
         info!("No valid event log file found.");
         return;
@@ -24,7 +29,13 @@ pub fn setup_redrive_event_log(sender: Res<FanOutEventSender>) {
     let reader = BufReader::new(file);
     for line in reader.lines().map_while(Result::ok) {
         if let Ok(logged) = serde_json::from_str::<LoggedEvent>(&line) {
-            let _ = sender.tx_fan_out_events.try_send(logged.event);
+            info!("Replaying event: {:?}", logged);
+            apply_event(
+                &logged.event,
+                &mut server_game_state,
+                &mut server_history_state,
+                &mut needs_world_broadcast,
+            );
         }
     }
 
